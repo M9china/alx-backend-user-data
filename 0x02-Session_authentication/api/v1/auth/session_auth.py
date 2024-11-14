4541fbffcb9e112a15d9a2f5aeb3d4bf1dba72d6
M9china <nqobilencaza2@gmail.com>
2024-11-14 14:38:12 +0200

    feat: add create_session method to SessionAuth for session ID management

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,5 +2,17 @@
 """Session Authentication Class"""
 from api.v1.auth.auth import Auth
 
+
 class SessionAuth(Auth):
     """Session Authentication Class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Create a session ID"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
