38eda83f7778218a07dc6d16c9e3e98ae3da46c1
M9china <nqobilencaza2@gmail.com>
2024-11-14 14:12:50 +0200

    feat: add current_user method to retrieve User instance from request

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,3 +56,22 @@ class BasicAuth(Auth):
             return user
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Overloads Auth and retrieves the User instance for a request"""
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+        base64_auth_header = self.extract_base64_authorization_header(
+            auth_header)
+        if base64_auth_header is None:
+            return None
+        decoded_auth_header = self.decode_base64_authorization_header(
+            base64_auth_header)
+        if decoded_auth_header is None:
+            return None
+        user_credentials = self.extract_user_credentials(decoded_auth_header)
+        if user_credentials is None:
+            return None
+        return self.user_object_from_credentials(
+            user_credentials[0], user_credentials[1])
