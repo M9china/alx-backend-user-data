fc5e63d7e07788a8b7cff3973a09d3ca83f8c319
M9china <nqobilencaza2@gmail.com>
2024-11-14 19:37:57 +0200

    feat: add RedactingFormatter class for enhanced logging with data redaction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Filtering module
 """
+import logging
 import re
+from typing import List
 
 
 def filter_datum(fields: list, redaction: str,
@@ -9,3 +11,32 @@ def filter_datum(fields: list, redaction: str,
     """returns the log message obfuscated"""
     return re.sub(r'(?<={}=).*?(?={})'.format(
         fields[0], separator), redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
