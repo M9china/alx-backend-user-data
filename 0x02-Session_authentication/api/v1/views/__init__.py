bd637fefe6d8774049d956d390332cd4f048b10b
M9china <nqobilencaza2@gmail.com>
2024-11-15 12:28:16 +0200

    fix: register session_auth blueprint correctly

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -2,10 +2,10 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
-from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_auth import session_auth
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
-app_views.register_blueprint(SessionAuth)
+app_views.register_blueprint(session_auth)
 
 from api.v1.views.index import *
 from api.v1.views.users import *
