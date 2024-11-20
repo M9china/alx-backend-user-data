761a01b23dd79e38664baa238d4f4ba01ca53d5f
M9china <nqobilencaza2@gmail.com>
2024-11-20 21:55:32 +0200

    feat: implement Auth class for user registration and password hashing

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 """hash password module"""
 import bcrypt
+from db import DB
+from user import User
 
 
-def _hash_password(password: str) -> bytes:
-    """Takes in a password string arguments and returns a salted, hashed
-    password, which is a byte string."""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+class Auth():
+    """Auth class to interact with the authentication database.
+    """
+    def __init__(self):
+        """Initialize the Auth instance."""
+        self._db = DB()
+
+    def _hash_password(self, password: str) -> bytes:
+        """Takes in a password string arguments and returns a salted, hashed
+        password, which is a byte string."""
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+    def register_user(self, password: str, email: str) -> User:
+        """Takes in password and email and returns user"""
+        try:
+            existing_user = self._db.find_user_by(email=email)
+            if existing_user:
+                raise ValueError(f'User {email} already exists')
+        except Exception:
+            hashed_password = self._hash_password(password)
+            user = self._db.add_user(email, hashed_password)
+            return user
