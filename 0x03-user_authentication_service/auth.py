4fa58488624cf1707350dc0a500ec65986d56245
M9china <nqobilencaza2@gmail.com>
2024-11-20 23:44:41 +0200

    feat: add valid_login method to Auth class for user authentication

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,6 +9,7 @@ from sqlalchemy.exc import IntegrityError
 class Auth():
     """Auth class to interact with the authentication database.
     """
+
     def __init__(self):
         """Initialize the Auth instance."""
         self._db = DB()
@@ -31,3 +32,15 @@ class Auth():
         hashed_password = self._hash_password(password)
         new_user = self._db.add_user(email, hashed_password)
         return new_user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Check if the provided password is valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user and bcrypt.checkpw(password.encode('utf-8'),
+                                       user.hashed_password):
+                return True
+
+        except Exception:
+            pass
+        return False
