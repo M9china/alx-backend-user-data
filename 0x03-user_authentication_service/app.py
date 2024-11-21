659f6435e40634e46eef9461ca209bf90db175ae
M9china <nqobilencaza2@gmail.com>
2024-11-21 11:43:56 +0200

    feat: add route to generate reset password token for users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -69,5 +69,19 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    """route to get reset password token"""
+    email = request.form.get('email')
+    if not email:
+        return flask.abort(403)
+
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        return jsonify({"message": "email not registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
