07c574a3f1c900486ca5b67289d2775f9987ecff
M9china <nqobilencaza2@gmail.com>
2024-11-21 10:59:17 +0200

    fix: update sessions route to enable strict slashes for consistency

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -33,7 +33,7 @@ def users():
         return jsonify({"message": "email already registered"}), 400
 
 
-@app.route('/sessions', methods=['POST'], strict_slashes=False)
+@app.route('/sessions', methods=['POST'], strict_slashes=True)
 def login():
     """route to login a user"""
     email = request.form.get('email')
