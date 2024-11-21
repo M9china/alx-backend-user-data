ad82e1a3435c83047e3174cf6d50323ee784416f
M9china <nqobilencaza2@gmail.com>
2024-11-21 11:05:04 +0200

    feat: add method to retrieve user from session ID in Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,3 +62,13 @@ class Auth():
         session_id = self._generate_uuid()
         self._db.update_user(user.id, session_id=session_id)
         return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """Get user from session id"""
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except Exception:
+            return None
