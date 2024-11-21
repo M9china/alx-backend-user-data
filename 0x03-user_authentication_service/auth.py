829c1a67c256c4b11b7c27551717db0e1617dcb8
M9china <nqobilencaza2@gmail.com>
2024-11-21 11:53:00 +0200

    feat: add method to update user password using reset token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -84,3 +84,13 @@ class Auth():
             return reset_token
         except Exception:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """Update password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = self._hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password)
+            self._db.update_user(user.id, reset_token=None)
+        except Exception:
+            raise ValueError
